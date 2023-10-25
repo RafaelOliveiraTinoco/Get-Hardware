087ad8a6f7aa9ead4c123f9d9c77f35052d0bf21
Rafael Oliveira Tinoco <rafaeloliveiratinoco33@gmail.com>
2023-10-25 10:03:42 +0100

    admin login page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,1 +1,16 @@
-abc
+@include("page-layout.start")
+
+<!-- Login Form -->
+<section class="bg-secondary w-25 position-absolute top-50 start-50 translate-middle p-5">
+
+    <p class="h1 text-white">Admin Panel</p>
+
+    <form method="POST" action="/admin/login">
+        <input type="text" class="form-control custom-input mt-3" placeholder="Username" name="username">
+        <input type="password" class="form-control custom-input mt-3" placeholder="Password" name="password">
+        <button type="submit" class="btn btn-primary text-white rounded-0 mt-5">Login</button>
+    </form>
+
+</section>
+
+@include("page-layout.end")
