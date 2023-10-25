3b794885ea3543f62cc9f6f7a9ddd805f2805b49
Rafael Oliveira Tinoco <rafaeloliveiratinoco33@gmail.com>
2023-10-25 10:44:09 +0100

    admin login methods completed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,18 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function loginPage(Request $request){
-        return view("admin.login");
+        if(self::isAuthenticated()) return view("admin.panel");
+        else return view("admin.login");
     }
 
-    public function login(Request$request){
-        return 0;
+    public function login(Request $request){
+        $request->validate([
+            "email" => ["email", "required"],
+            "password" => ["email", "required"],
+        ]);
+
+        $credentials = $request->only("email", "password");
+
+        if(Auth::guard("admin")->attempt($credentials)) return redirect()->back(); // if login successful go back to /admin route
+        else return redirect()->back()->withErrors(["email" => "Invalid credentials!"]); // else go back to /admin route with errors
     }
 
-    public function logout(Request$request){
-        return 0;
+    public function logout(Request $request){
+        Auth::guard("admin")->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect("/admin");
+    }
+
+    public static function isAuthenticated(){
+        return Auth::guard("admin")->check(); // returns true if the user is authenticated, and false if the user is not authenticated
     }
 }
