9fd5d72d7f14c34d59e1da919ca0e77e9446bd27
Rafael Oliveira Tinoco <rafaeloliveiratinoco33@gmail.com>
2023-10-25 10:13:53 +0100

    created login methods

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,4 +9,12 @@ class AdminController extends Controller
     public function loginPage(Request $request){
         return view("admin.login");
     }
+
+    public function login(Request$request){
+        return 0;
+    }
+
+    public function logout(Request$request){
+        return 0;
+    }
 }
